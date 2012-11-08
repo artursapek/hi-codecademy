f686c823a0d95d150507533116399810db12edbb
Artur Sapek <artur.sapek@gmail.com>
2012-11-08 17:56:48 -0500

    Create db and start on the UI controls

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 urlpatterns = patterns('',
+    url(r'^assets/(?P<path>.*)$', 'django.views.static.serve',
+      {'document_root': settings.STATIC_ROOT}),
     url(r'$', 'src.views.index'),
 )
